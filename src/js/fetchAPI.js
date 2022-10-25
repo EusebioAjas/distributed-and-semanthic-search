c432fa90c39e0e9ed255074e093f0e0b995f9eab
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-10-24 23:09:25 -0500

    get data from both apis

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,5 +1,7 @@
 const DATAMUSE_URL = 'https://api.datamuse.com/words?ml=';
+const EUROPEAN_ARTICLE = 'https://www.europeana.eu/es/item';
 const EUROPEAN_URL = 'https://api.europeana.eu/record/v2/search.json?reusability=open&media=true&wskey=actocksh&query=';
+const PLOS_ARTICLE = 'https://journals.plos.org/plosone/article?id=';
 const PLOS_URL = 'http://api.plos.org/search?q=title:';
 
 const EUROPEAN = "European";
@@ -20,13 +22,52 @@ async function getDataFrom(API_URL, fn) {
 }
 
 async function handleSearchBtn() {
-  const result = await getDataFrom(PLOS_URL, fetchResolver);
-  const docs = result.response.docs;
-  docs.map((doc) => {
-    let originalScore = doc.score;
-    let normalized = doc.score / result.maxScore;
-    renderItem(doc.id, PLOS, originalScore, normalized);
+  const result1 = await mapDataSourceFromEuropean();
+  const result2 = await mapDataSourceFromPLOS();
+  const finalResult = result1.concat(result2);
+
+  console.log(finalResult);
+  return finalResult;
+}
+
+async function mapDataSourceFromEuropean() {
+  const result = await getDataFrom(EUROPEAN_URL, fetchResolver);
+  const arrData = result.items;
+  const maxScore = (arrData.length > 0 && arrData[0].score) ? arrData[0].score : 1;
+  const data = arrData.map((item) => {
+    return getObjectFromEuropean(item, maxScore);
   })
+  return data;
+}
+
+async function mapDataSourceFromPLOS() {
+  const result = await getDataFrom(PLOS_URL, fetchResolver);
+  const arrData = result.response.docs;
+  const maxScore = result.response.maxScore;
+  const data = arrData.map((item) => {
+    return getObjectFromPLOS(item, maxScore);
+  })
+  return data;
+}
+
+function getObjectFromEuropean(item, maxScore) {
+  let doc = {};
+  doc.link = PLOS_ARTICLE + item.id;
+  doc.normalizedScore = item.score / maxScore;
+  doc.originalScore = item.score;
+  doc.provider = PLOS;
+  doc.title = item.title[0];
+  return doc;
+}
+
+function getObjectFromPLOS(item, maxScore) {
+  let doc = {};
+  doc.link = EUROPEAN_ARTICLE + item.id;
+  doc.normalizedScore = item.score / maxScore;
+  doc.originalScore = item.score;
+  doc.provider = EUROPEAN;
+  doc.title = item.title_display;
+  return doc;
 }
 
 async function getAllData() {
@@ -36,32 +77,17 @@ async function getAllData() {
   const data = await Promise.all([datamuse, european, plos]);
 
   return data;
-
 }
 
-function parseProvider(response) {
-  const res = JSON.parse(response);
-}
-
-function plosParser({ doc }) {
-  const score = doc.score;
-  const link = doc.title;
-
-  return { score, link, PLOS};
-}
-
-function parser({ item }) {
-  const link = item.link;
-  const originalScore = item.score;
-
-  return { link, originalScore, EUROPEAN };
-}
-
-function renderItem({ link, provider, originalValue, normalizedValue }) {
-  return `<div>
+function renderItem(link, provider, originalValue, normalizedValue) {
+  return `<div class="card">
     <p>Provider: ${provider}</p>
-    <a href="${link}">Link</a>
+    <p><a href="${link}">Link</a></p>
     <p>Original value: ${originalValue}</p>
     <p>normalized value: ${normalizedValue}</p>
   </div>`;
+}
+
+function compareScore(score1, score2) {
+  return score2 - score1;
 }
